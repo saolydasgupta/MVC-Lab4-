f4fc0a9faf137f76c5bbe1f7b0c17f513951c146
saolydasgupta <50455874+saolydasgupta@users.noreply.github.com>
2021-03-23 14:53:51 +0000

    Add files via upload

---
+++ CarController.cs
@@ -0,0 +1,19 @@
+﻿using Car.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Car.Controllers
+{
+    public class CarController : Controller
+    {
+        public IActionResult Index()
+
+        {
+            Vehicle myCar = new Vehicle() {Make = "Toyota", Model = "Yaris", EngineSize = 20, Engine = EngineType.Hybrid };
+            return View(myCar);
+        }
+    }
+}
---
+++ Vehicle.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Car.Models
+{
+    public enum EngineType { Petrol, Diesel, Hybqrid, Electric,
+        Hybrid
+    }
+    public class Vehicle
+    {
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public int EngineSize { get; set; }
+        public EngineType Engine { get; set; }
+
+    }
+}
